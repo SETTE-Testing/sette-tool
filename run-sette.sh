9e07153ff4db6ef33395bd77fd26425b17389106
Lajos Cseppentő <cseppento@outlook.com>
2016-01-24 11:02:38 +0100

    Replace log4j2 by logback-classic, update guava, jackson, javaparser-core dependency versions, commit sonarqube support

--- run-sette.sh
+++ run-sette.sh
@@ -7,12 +7,4 @@ CWD="$(
 )"
 
 cd "$CWD"
-
-VMARGS=
-
-if [ -f "$CWD/sette-log4j2.xml" ]; then
-    VMARGS=-Dlog4j.configurationFile=sette-log4j2.xml
-fi
-
-java $VMARGS -jar "$CWD/sette-all.jar" "$@"
-
+java -jar "$CWD/sette-all.jar" "$@"
