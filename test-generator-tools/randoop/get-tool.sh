4eb358e74a0c7835022cf29962c547cd3713a603
Lajos Cseppentő <cseppento@outlook.com>
2016-03-08 09:02:41 +0100

    Update randoop version to 2.1.4

--- test-generator-tools/randoop/get-tool.sh
+++ test-generator-tools/randoop/get-tool.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # This script downloads Randoop.
-
+VERSION=2.1.4
 CWD="$(
   cd "$(dirname "$(readlink "$0" || printf %s "$0")")"
   pwd -P 
 )"
 
 rm -f "$CWD/randoop.jar"
-wget "https://github.com/mernst/randoop/releases/download/v1.3.6/randoop-1.3.6.jar" -O "$CWD/randoop.jar"
+wget "https://github.com/mernst/randoop/releases/download/v$VERSION/randoop-$VERSION.jar" -O "$CWD/randoop.jar"
 chmod +x "$CWD/randoop.jar"
 
-echo "1.3.6" > "$CWD/VERSION"
+echo "$VERSION" > "$CWD/VERSION"
