4ee92ec6c8e94fc11cbda72c75ef6f500543f601
Zoltan Micskei <micskeiz@mit.bme.hu>
2016-03-11 15:52:58 +0100

    Rewrote script: fixed SETTE parameters, added script parameters, added documentation comment

--- experiment-evaluate.ps1
+++ experiment-evaluate.ps1
@@ -1,32 +1,43 @@
-﻿$snippetProject = "sette-snippets"
-# $snippetProject = "sette-snippets-performance-time"
+﻿<#
+
+.SYNOPSIS
+Runs the evaluation tasks on all tools for the 30sec results.
+
+#>
+
+[CmdletBinding()]
+
+Param(
+  [int] $From = 1,
+  [int] $To = 10,
+  [string] $JavaHeapMemory = "4G"
+)
+
+$SNIPPET_PROJECT = "sette-snippets"
+$SNIPPET_PROJECT_DIR = "sette-snippets/java/sette-snippets"
+$LOG_DIR = "explog"
 
 $tools = @("catg", "evosuite", "jpet", "randoop", "spf")
-# $tools = @("evosuite")
-
-$from =  1
-$to   = 10
 
 $tags = @()
-for ($i = $from; $i -le $to; $i++) {  
+for ($i = $From; $i -le $To; $i++) {  
     $tags += "run-{0:D2}-30sec" -f $i
 }
 
 $tasks = @("parser", "test-generator", "test-runner", "export-csv")
 
-mkdir explog -f > $null
+mkdir $LOG_DIR -f > $null
 
 foreach ($tool in $tools) {
     foreach ($tag in $tags) {
         $i = 3
         foreach ($task in $tasks) {
             echo "$tool $tag $task ..."
-            java -Xmx4G -jar sette-all.jar --snippet-project $snippetProject --tool $tool --task $task --runner-project-tag $tag > "explog/${tool}_${tag}_${i}_${task}.log" 2>&1
+            java "-Xmx$JavaHeapMemory" -jar sette-all.jar --snippet-project-dir $SNIPPET_PROJECT_DIR --tool $tool --task $task --runner-project-tag $tag > "$LOG_DIR/${tool}_${tag}_${i}_${task}.log" 2>&1
             $i++
         }
     }
 }
 
-$tool = $tools -join ','
 $tag = $tags -join ','
-java -jar sette-all.jar --snippet-project $snippetProject --tool $tool --task "export-csv-batch" --runner-project-tag $tag
+java -jar sette-all.jar --snippet-project-dir $SNIPPET_PROJECT_DIR --task "export-csv-batch" --runner-project-tag $tag
