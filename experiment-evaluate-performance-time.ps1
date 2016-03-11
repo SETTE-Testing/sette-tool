daf7a1d8a164d6286db31bb23e0170e12d070bb7
Zoltan Micskei <micskeiz@mit.bme.hu>
2016-03-11 15:30:16 +0100

    Rewrote perf-time evalute PS script: fixed SETTE param names, uses CommonParameters, changed variable names, added MergeCsv switch

--- experiment-evaluate-performance-time.ps1
+++ experiment-evaluate-performance-time.ps1
@@ -1,10 +1,24 @@
-﻿$snippetProject = "sette-snippets-performance-time"
-$simulate = $false # does not will call SETTE
-#$simulate = $true
+﻿[CmdletBinding()]
+
+Param(
+  [switch] $MergeCsv
+)
+
+$SNIPPET_PROJECT = "sette-snippets-performance-time"
+$SNIPPET_PROJECT_DIR = "sette-snippets/java/sette-snippets-performance-time"
+$LOG_DIR = "explog"
 
 $tasks = @{"3" = "parser"; "4" =  "test-generator"; "5" = "test-runner"; "6" = "export-csv"}
 
 $targets = @{
+    "catg" = @{
+        "1|15sec" = (1..10);
+        "2|45sec" = (1..10);
+        "3|60sec" = (1..10);
+        "4|120sec" = (1..1);
+        "5|180sec" = (1..1);
+        "6|300sec" = (1..10);
+    };
     "evosuite" = @{
         "1|15sec" = (1..10);
         "2|45sec" = (1..10);
@@ -23,8 +37,8 @@ $targets = @{
     }
 }
 
-mkdir "explog" -f > $null
-mkdir "explog/$snippetProject" -f > $null
+mkdir "$LOG_DIR" -f > $null
+mkdir "$LOG_DIR/$SNIPPET_PROJECT" -f > $null
 
 $csvs = @()
 
@@ -35,38 +49,40 @@ foreach ($tool in $targets.Keys | Sort-Object) {
         foreach($num in $targets[$tool][$timeKey]) {
             $num = "{0:D2}" -f $num
             $tag = "run-${num}-${time}"
-            $dir = "${snippetProject}___${tool}___${tag}"
+            $dir = "${SNIPPET_PROJECT}___${tool}___${tag}"
+
+			if (!(Test-Path "../sette-results/$dir")) {
+                Write-Warning "Missing $dir"
+                continue
+			}
 
             $csvs += "../sette-results/$dir/sette-evaluation.csv"
 
-            if (!(Test-Path "../sette-results/$dir")) {
-                echo "Missing $dir"
-                exit
-            } elseif (Test-Path "../sette-results/$dir/sette-evaluation.csv") {
+            if (Test-Path "../sette-results/$dir/sette-evaluation.csv") {
                 Write-Verbose "Skipping $tool $tag"
             } else {
                 foreach ($taskNum in $tasks.Keys | Sort-Object) {
                     $task = $tasks[$taskNum]
 
-                    echo "$snippetProject $tool $tag $task ..."
-                    if (!$simulate) {
-                        java -Xmx4G -jar sette-all.jar --snippet-project $snippetProject --tool $tool --task $task --runner-project-tag $tag > "explog/$snippetProject/${tool}_${tag}_${taskNum}_${task}.log" 2>&1
-                    }
+                    Write-Verbose "$SNIPPET_PROJECT $tool $tag $task ..."
+                    if (! $WhatIfPreference) {
+                        java -Xmx4G -jar sette-all.jar --snippet-project $SNIPPET_PROJECT_DIR --tool $tool --task $task --runner-project-tag $tag > "$LOG_DIR/$SNIPPET_PROJECT/${tool}_${tag}_${taskNum}_${task}.log" 2>&1
+                    } else {
+						echo "java -Xmx4G -jar sette-all.jar --snippet-project $SNIPPET_PROJECT_DIR --tool $tool --task $task --runner-project-tag $tag > $LOG_DIR/$SNIPPET_PROJECT/${tool}_${tag}_${taskNum}_${task}.log 2>&1"
+                    }                    
                 }
             }
         }
     }
 }
 
-#$tool = $tools -join ','
-#$tag = $tags -join ','
-#java -jar sette-all.jar --snippet-project $snippetProject --tool $tool --task "export-csv-batch" --runner-project-tag $tag
+if ($MergeCsv) {
+	Write-Verbose "Merging csvs"
+	$mergedCsv = @()
 
-echo "Merging csvs"
-$mergedCsv = @()
+	foreach ($csv in $csvs) {
+		$mergedCsv += Import-Csv $csv
+	}
 
-foreach ($csv in $csvs) {
-    $mergedCsv += Import-Csv $csv
+	$mergedCsv | Export-Csv "performance-time.csv"
 }
-
-$mergedCsv | Export-Csv "performance-time.csv"
