c80ef37b67a8fec442164cabce0cb9d608d67136
Zoltan Micskei <micskeiz@mit.bme.hu>
2016-03-16 12:35:04 +0100

    Corrected tab/space indentation

--- experiment-evaluate.ps1
+++ experiment-evaluate.ps1
@@ -1,7 +1,7 @@
 ﻿<#
 
 .SYNOPSIS
-Runs the evaluation tasks on all tools for the 30sec results.
+Runs the evaluation tasks on selected tools for the 30sec results.
 
 #>
 
@@ -23,29 +23,29 @@ $LOG_DIR = "explog"
 
 $tags = @()
 for ($i = $From; $i -le $To; $i++) {  
-    $tags += "run-{0:D2}-30sec" -f $i
+  $tags += "run-{0:D2}-30sec" -f $i
 }
 
 mkdir $LOG_DIR -f > $null
 
 foreach ($tool in $Tools) {
-    foreach ($tag in $tags) {
-		$dir = "${SNIPPET_PROJECT}___${tool}___${tag}"
-		
-		if ($SkipExisting -and (Test-Path "../sette-results/$dir/sette-evaluation.csv")) {
-            Write-Warning "Skipping $tool $tag"
-        } else {
-			$i = 3
-			foreach ($task in $tasks) {
-				Write-Progress -Activity $tool -Status $tag -CurrentOperation $task
-				java "-Xmx$JavaHeapMemory" -jar sette-all.jar --snippet-project-dir $SNIPPET_PROJECT_DIR --tool $tool --task $task --runner-project-tag $tag > "$LOG_DIR/${tool}_${tag}_${i}_${task}.log" 2>&1
-				$i++			
-			}
-        }
+  foreach ($tag in $tags) {
+    $dir = "${SNIPPET_PROJECT}___${tool}___${tag}"
+    
+    if ($SkipExisting -and (Test-Path "../sette-results/$dir/sette-evaluation.csv")) {
+      Write-Warning "Skipping $tool $tag"
+    } else {
+      $i = 3
+      foreach ($task in $tasks) {
+        Write-Progress -Activity $tool -Status $tag -CurrentOperation $task
+        java "-Xmx$JavaHeapMemory" -jar sette-all.jar --snippet-project-dir $SNIPPET_PROJECT_DIR --tool $tool --task $task --runner-project-tag $tag > "$LOG_DIR/${tool}_${tag}_${i}_${task}.log" 2>&1
+        $i++
+      }
     }
+  }
 }
 
 if ($ExportCsvBatch){
-	$tag = $tags -join ','
-	java -jar sette-all.jar --snippet-project-dir $SNIPPET_PROJECT_DIR --task "export-csv-batch" --runner-project-tag $tag
+  $tag = $tags -join ','
+  java -jar sette-all.jar --snippet-project-dir $SNIPPET_PROJECT_DIR --task "export-csv-batch" --runner-project-tag $tag
 }
