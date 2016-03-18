5a2190f0cfb44427b00bf09b6eeb969554bd9223
Zoltan Micskei <micskeiz@mit.bme.hu>
2016-03-18 15:59:49 +0100

    Updated evaluate scripts with AntOptions parameters and Write-Progress

--- experiment-evaluate-performance-time.ps1
+++ experiment-evaluate-performance-time.ps1
@@ -9,12 +9,15 @@ Runs the evaluation tasks on hard-coded performance-time results.
 
 Param(
   [switch] $MergeCsv,
-  [string] $JavaHeapMemory = "4G"
+  [string] $JavaHeapMemory = "4G",
+  [string] $AntOptions = "-Xmx4g"
 )
 
 $SNIPPET_PROJECT = "sette-snippets-performance-time"
 $SNIPPET_PROJECT_DIR = "sette-snippets/java/sette-snippets-performance-time"
 $LOG_DIR = "explog"
+
+$Env:ANT_OPTS = $AntOptions
 
 $tasks = @{"3" = "parser"; "4" =  "test-generator"; "5" = "test-runner"; "6" = "export-csv"}
 
@@ -23,24 +26,32 @@ $targets = @{
         "1|15sec" = (1..10);
         "2|45sec" = (1..10);
         "3|60sec" = (1..10);
-        "4|120sec" = (1..1);
-        "5|180sec" = (1..1);
+#        "4|120sec" = (1..1);
+#        "5|180sec" = (1..1);
+        "6|300sec" = (1..10);
+    };
+    "jpet" = @{
+        "1|15sec" = (1..10);
+        "2|45sec" = (1..10);
+        "3|60sec" = (1..10);
+#        "4|120sec" = (1..1);
+#        "5|180sec" = (1..1);
         "6|300sec" = (1..10);
     };
     "evosuite" = @{
         "1|15sec" = (1..10);
         "2|45sec" = (1..10);
         "3|60sec" = (1..10);
-        "4|120sec" = (1..1);
-        "5|180sec" = (1..1);
+#        "4|120sec" = (1..1);
+#        "5|180sec" = (1..1);
         "6|300sec" = (1..10);
     };
-    "randoop" = @{
+    "spf" = @{
         "1|15sec" = (1..10);
         "2|45sec" = (1..10);
         "3|60sec" = (1..10);
-        "4|120sec" = (1..1);
-        "5|180sec" = (1..1);
+#        "4|120sec" = (1..1);
+#        "5|180sec" = (1..1);
         "6|300sec" = (1..10);
     }
 }
@@ -59,26 +70,21 @@ foreach ($tool in $targets.Keys | Sort-Object) {
             $tag = "run-${num}-${time}"
             $dir = "${SNIPPET_PROJECT}___${tool}___${tag}"
 
-			if (!(Test-Path "../sette-results/$dir")) {
+            if (!(Test-Path "../sette-results/$dir")) {
                 Write-Warning "Missing $dir"
                 continue
-			}
+            }
 
             $csvs += "../sette-results/$dir/sette-evaluation.csv"
 
             if (Test-Path "../sette-results/$dir/sette-evaluation.csv") {
-                Write-Verbose "Skipping $tool $tag"
+                Write-Output "Skipping $tool $tag"
             } else {
                 foreach ($taskNum in $tasks.Keys | Sort-Object) {
                     $task = $tasks[$taskNum]
 
-                    Write-Verbose "$SNIPPET_PROJECT $tool $tag $task ..."
-                    #FIXME no whatif in PS 5 for scripts?
-                    if (! $WhatIfPreference) {
-                        java -Xmx$JavaHeapMemory -jar sette-all.jar --snippet-project-dir $SNIPPET_PROJECT_DIR --tool $tool --task $task --runner-project-tag $tag > "$LOG_DIR/$SNIPPET_PROJECT/${tool}_${tag}_${taskNum}_${task}.log" 2>&1
-                    } else {
-						echo "java -Xmx$JavaHeapMemory -jar sette-all.jar --snippet-project $SNIPPET_PROJECT_DIR --tool $tool --task $task --runner-project-tag $tag > $LOG_DIR/$SNIPPET_PROJECT/${tool}_${tag}_${taskNum}_${task}.log 2>&1"
-                    }                    
+                    Write-Progress -Activity "$SNIPPET_PROJECT $tool" -Status $tag -CurrentOperation $task
+                    java "-Xmx$JavaHeapMemory" -jar sette-all.jar --snippet-project-dir $SNIPPET_PROJECT_DIR --tool $tool --task $task --runner-project-tag $tag > "$LOG_DIR/$SNIPPET_PROJECT/${tool}_${tag}_${taskNum}_${task}.log" 2>&1                    
                 }
             }
         }
@@ -86,12 +92,12 @@ foreach ($tool in $targets.Keys | Sort-Object) {
 }
 
 if ($MergeCsv) {
-	Write-Verbose "Merging csvs"
-	$mergedCsv = @()
-
-	foreach ($csv in $csvs) {
-		$mergedCsv += Import-Csv $csv
-	}
-
-	$mergedCsv | Export-Csv "performance-time.csv"
+    Write-Progress "Merging csvs"
+    $mergedCsv = @()
+    
+    foreach ($csv in $csvs) {
+        $mergedCsv += Import-Csv $csv
+    }
+    
+    $mergedCsv | Export-Csv "performance-time.csv" -NoTypeInformation
 }
