90d9a1f7bdea12f48ce81de4d2025a4c4d7bd59e
Lajos Cseppentő <cseppento@outlook.com>
2016-04-14 23:17:04 +0200

    checkLicense task for buildscript and fix file license comments

--- src/sette-common/src/hu/bme/mit/sette/common/annotations/SetteAnnotation.java
+++ src/sette-common/src/hu/bme/mit/sette/common/annotations/SetteAnnotation.java
@@ -20,7 +20,6 @@
  * express or implied. See the License for the specific language governing permissions and 
  * limitations under the License.
  */
-
 package hu.bme.mit.sette.common.annotations;
 
 import java.lang.annotation.ElementType;
